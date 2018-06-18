ce45d07172fd0452bd3b0e5958009b559ae7d59f
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-06-18 10:19:03 +0530

    Add a mechanism to act on conversations:  - Create a WeakMap to hold DOM attached conversations  - Handle reply additions on click event on the reply button

--- scripts/load.js
+++ scripts/load.js
@@ -1,17 +1,31 @@
+const Threading = new WeakMap();
 window.addEventListener( 'DOMContentLoaded', ( e ) => {
   const actor = document.querySelector( '.add-comment' );
-  actor.addEventListener( 'click', ( e ) => {
-    postComment();
+  actor.addEventListener( 'click', e => {
+    return postComment();
   } );
   const postArea = document.querySelector( '#posts' );
-  postArea.addEventListener( 'click', ( e ) => {
+  postArea.addEventListener( 'click', e => {
     const Target = e.target;
-    if ( Target.nodeName === 'A' ) {
+    if ( Target.nodeName === 'A' || Target.nodeName === 'BUTTON' ) {
       const Id = Target.getAttribute( 'id' );
       const postId = '#post_' + Id.split( '_' )
         .pop();
       const Thread = document.querySelector( postId + ' .replies' );
-      Thread.classList.toggle( 'open' );
+      switch ( Target.nodeName ) {
+      case "A":
+        const Reply = document.querySelector( postId + ' .reply' );
+        Thread.classList.toggle( 'open' );
+        Reply.classList.toggle( 'open' );
+        break;
+      case "BUTTON":
+        const Comment = document.querySelector( postId + ' #in-thread-comment' )
+          .value;
+        addReply( postId, Comment );
+        break;
+      default:
+        //Do nothing
+      }
     }
   } );
 } );
